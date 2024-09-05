e3f2e7475e6f514d8c589221f955aa3bac76c4d2
tanner johnson <tannerjohnson08@gmail.com>
2024-09-05 11:24:35 -0500

    added components folder

---
+++ client/src/components/Header.jsx
@@ -0,0 +1,20 @@
+
+
+export default function Header(){
+
+
+
+    return (
+        <nav>
+
+        <div>
+            <a>Search</a>
+        </div>
+
+        <div>
+            <a>Profile</a>
+        </div>
+
+        </nav>
+    )
+}
