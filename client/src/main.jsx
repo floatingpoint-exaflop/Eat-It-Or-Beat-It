4cebc711e5bd5435a42bc4ade2b1cf44978d141a
tanner johnson <tannerjohnson08@gmail.com>
2024-09-06 11:21:25 -0500

    routing started

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,17 +1,13 @@
+import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 import App from './App.jsx';
 import Home from './pages/Home';
-import RecipePage from './pages/RecipePage.jsx';
+import RecipePage from './pages/Result.jsx';
 
 createRoot(document.getElementById('root')).render(
-  <Router>
-    <App>
-      <Routes>
-        <Route exact path="/" element={<Home />} />
-        <Route path="/recipe" element={<RecipePage />} />
-      </Routes>
-    </App>
-  </Router>
+  <StrictMode>
+    <App />
+  </StrictMode>
 );
