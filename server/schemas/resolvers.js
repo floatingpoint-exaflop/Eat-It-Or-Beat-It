f1a830049ac9c82fe904567c40b6dfa7cfb9a2cc
Frederico Swarthings <timscallon1@gmail.com>
2024-09-08 18:09:07 -0500

    schemas

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,24 +1,85 @@
-const { User, Recipe } = require('../models');
+const { User, Recipe, Comment } = require('../models');
 
 const resolvers = {
   Query: {
-    user: async () => {
-
+    // Fetch all recipes for a user
+    getRecipes: async (parent, { userId }) => {
+      const user = await User.findById(userId).populate('recipes');
+      return user.recipes;
     },
-    recipes: async (parent, { _id }) => {
 
+    // Fetch a single recipe by ID for a user
+    getSingleRecipe: async (parent, { recipeId, userId }) => {
+      const user = await User.findById(userId).populate({
+        path: 'recipes',
+        match: { _id: recipeId },
+      });
+      return user.recipes[0];
+    },
+
+    // Fetch all comments for a specific recipe
+    getComments: async (parent, { recipeId }) => {
+      return Comment.find({ recipe: recipeId });
+    },
+
+    // Fetch a single comment by ID
+    getSingleComment: async (parent, { commentId }) => {
+      return Comment.findById(commentId);
+    },
+
+    // Fetch a single user by ID
+    getSingleUser: async (parent, { userId }) => {
+      return User.findById(userId).populate('recipes');
     },
   },
+
   Mutation: {
-    createUser: async (parent, args) => {
-
+    // Add a recipe for a user
+    addRecipe: async (parent, { userId, recipeData }) => {
+      const recipe = await Recipe.create(recipeData);
+      await User.findByIdAndUpdate(userId, { $push: { recipes: recipe._id } });
+      return recipe;
     },
-    // createRecipe: async (parent, { _id, recipe }) => {
 
-    // },
+    // Add a comment to a recipe by a user
+    addComment: async (parent, { recipeId, userId, commentBody, rating }) => {
+      const comment = await Comment.create({
+        commentBody,
+        rating,
+        recipe: recipeId,
+        user: userId,
+      });
+      return comment;
+    },
+
+    // Add a new user
+    addUser: async (parent, { username, email, password }) => {
+      const user = await User.create({ username, email, password });
+      return user;
+    },
+  },
+
+  //Resolvers for fields that need to reference other models
+  Recipe: {
+    comments: async (parent) => {
+      return Comment.find({ recipe: parent._id });
+    },
+  },
+
+  User: {
+    recipes: async (parent) => {
+      return Recipe.find({ _id: { $in: parent.recipes } });
+    },
+  },
+
+  Comment: {
+    user: async (parent) => {
+      return User.findById(parent.user);
+    },
+    recipe: async (parent) => {
+      return Recipe.findById(parent.recipe);
+    },
   },
 };
 
 module.exports = resolvers;
-//Replace these blank queries with get requests for graphQL
-//Replace these blank mutations with post/put/delete requests for graphQL
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,22 +1,128 @@
 const typeDefs = `
-  type User {
-    _id: ID!
-    name: String!
+type User {
+  _id: ID!
+  username: String!
+  email: String!
+  password: String!
+  recipes: [Recipe] # User's saved recipes
+}
+
+  input CreateUserInput {
+    username: String!
+    email: String!
+    password: String!
   }
 
-  type Recipe {
-    _id: ID!
+
+type Recipe {
+  _id: ID!
+  api_recipe_id: Int!
+  recipe_name: String!
+  recipe_description: String
+  recipe_image: String
+  recipe_ingredients: RecipeIngredients
+  recipe_nutrition: RecipeNutrition
+  directions: [Direction]
+  ingredients: [Ingredient]
+  grams_per_portion: String
+  number_of_servings: String
+  rating: String
+  serving_sizes: ServingSizes
+  recipe_types: [String]
+}
+  type RecipeIngredients {
+    ingredient: [String] # List of ingredient names
+  }
+  type RecipeNutrition {
+    calories: String
+    carbohydrate: String
+    fat: String
+    protein: String
+  }
+  type Direction {
+    direction_description: String
+    direction_number: Int
+  }
+  type Ingredient {
+    food_id: String
+    food_name: String
+    ingredient_description: String
+    ingredient_url: String
+    measurement_description: String
+    number_of_units: String
+    serving_id: String
+  }
+  type ServingSizes {
+    serving_size: String
+    calories: String
+    carbohydrate: String
+    cholesterol: String
+    fat: String
+    fiber: String
+    iron: String
+    monounsaturated_fat: String
+    polyunsaturated_fat: String
+    potassium: String
+    protein: String
+    saturated_fat: String
+    sodium: String
+    sugar: String
+    trans_fat: String
+    vitamin_a: String
+    vitamin_c: String
   }
 
-  type Query {
-    user: [User]
-    recipes(_id: String): [Recipe]
-  }
+    input AddRecipeInput {
+      api_recipe_id: Int!
+      recipe_name: String!
+      recipe_description: String
+      recipe_image: String
+      recipe_ingredients: RecipeIngredientsInput
+      recipe_nutrition: RecipeNutritionInput
+    }
+    input RecipeIngredientsInput {
+      ingredient: [String]
+    }
+    input RecipeNutritionInput {
+      calories: String
+      carbohydrate: String
+      fat: String
+      protein: String
+    }
 
-  type Mutation {
-    createMatchup(sadf: String!, afsd: String!): Recipe
-    createUser(asdf: String!, asdff: Int!): Recipe
-  }
+
+type Comment {
+  _id: ID!
+  commentBody: String!
+  rating: Int!
+  api_recipe_id: Int!
+  user: User!
+  recipe: Recipe!
+  createdAt: String!
+  updatedAt: String!
+}
+input AddCommentInput {
+  commentBody: String!
+  rating: Int!
+  api_recipe_id: Int!
+  user: ID!
+  recipe: ID!
+}
+
+type Query {
+  getUsers: [User]
+  getUser(id: ID!): User
+  getRecipes: [Recipe]
+  getSingleRecipe(id: ID!): Recipe
+  getComments: [Comment]
+  getSingleComment(id: ID!): Comment
+}
+
+type Mutation {
+  addUser(input: CreateUserInput): User
+  addRecipe(input: AddRecipeInput): Recipe
+  addComment(input: AddCommentInput): Comment
+}
 `;
 
 module.exports = typeDefs;
