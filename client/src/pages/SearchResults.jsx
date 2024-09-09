f6fbc8525d3fbe18c13f8c49e42e2a3f19c5f985
Frederico Swarthings <timscallon1@gmail.com>
2024-09-09 17:27:58 -0500

    working on handling search data we've gotten back

---
+++ client/src/pages/SearchResults.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { useLocation } from "react-router-dom";
+import EatItOrBeatIt from '../components/EatItOrBeatIt.jsx';
+
+export default function SearchResults(recipeSearchResults) {
+  const location = useLocation();
+  const { results } = location.state || {};
+
+  return (
+    <div>
+      {results ? (
+        // <EatItOrBeatIt />
+        <EatItOrBeatIt recipes={recipeSearchResults} />
+      ) : (
+        <p>No search results available.</p>
+      )}
+    </div>
+  );
+};
